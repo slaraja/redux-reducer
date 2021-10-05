84c309e90b49aff4df191c15c6b1d95e3688718e
Stephanie La Raja <stephanielaraja@gmail.com>
2021-10-05 09:47:18 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
+let state = { friends: [] }
+
+manageFriends(state, {type: "ADD_FRIEND",
+    friend: {
+        name: "Chrome Boi",
+        hometown: "NYC",
+        id: 1,
+    },
+});
+
+manageFriends(state, { type: "REMOVE_FRIEND",
+id: 1, });
+
 export function manageFriends(state, action){
-}
+    
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return { friends: [...state.friends, action.friend] };
+        case "REMOVE_FRIEND":
+            return {friends: state.friends.filter(friend => friend.id !== action.id)}
+        default:
+            return state;
+        }
+
+    }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
+let state = { numberOfPresents: 0 }
+let action = { type: "INCREASE" }
+
 export function managePresents(state, action){
+    switch (action.type) {
+        case "INCREASE":
+            return { numberOfPresents: state.numberOfPresents + 1 };
+        default:
+            return state;
+    }
+    
 }
+
+
+
